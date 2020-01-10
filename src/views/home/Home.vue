c82bcf78bc86f0ea2f70d7e35b3593f84c59e899
Bob <x79016313@gmail.com>
2020-01-10 09:58:49 +0800

    新增輪播

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,6 +1,28 @@
 <template src="./Home.html"></template>
 <script>
 export default {
+    name:'Home',
+    data() {
+        return {
+            active:0,
+            imgList:[
+                require("@/assets/img/banner1.webp"),
+                require("@/assets/img/banner2.webp"),
+                require("@/assets/img/banner3.webp"),
+                require("@/assets/img/banner4.webp")
+            ]
+        }
+    },
+    computed: {
+        total() {
+            return this.imgList.length
+        }
+    },
+    methods: {
+        change(index) {
+            this.active = (index + this.total) % this.total
+        }
+    }
     
 }
 </script>

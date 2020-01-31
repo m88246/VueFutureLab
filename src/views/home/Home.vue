2481b441f2345d1775eacbed7294b08282d7658e
Bob <x79016313@gmail.com>
2020-01-31 10:00:39 +0800

    新增beforeDestroy取消輪播定時方法

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,35 +1,37 @@
 <template src="./Home.html"></template>
 <script>
 export default {
-    name:'Home',
-    data() {
-        return {
-            active:0,
-            timer:0,
-            imgList:[
-                require("@/assets/img/banner1.webp"),
-                require("@/assets/img/banner2.webp"),
-                require("@/assets/img/banner3.webp"),
-                require("@/assets/img/banner4.webp")
-            ]
-        }
-    },
-    computed: {
-        total() {
-            return this.imgList.length
-        }
-    },
-    methods: {
-        change(index) {
-            this.active = (index + this.total) % this.total
-        }
-    },
-    created() {
-        this.timer = setInterval(()=>{
-            this.active = (this.active+1)% this.total
-        },5000)
+  name: "Home",
+  data() {
+    return {
+      active: 0,
+      timer: 0,
+      imgList: [
+        require("@/assets/img/banner1.webp"),
+        require("@/assets/img/banner2.webp"),
+        require("@/assets/img/banner3.webp"),
+        require("@/assets/img/banner4.webp")
+      ]
+    };
+  },
+  computed: {
+    total() {
+      return this.imgList.length;
+    }
+  },
+  methods: {
+    change(index) {
+      this.active = (index + this.total) % this.total;
+    }
+  },
+  created() {
+    this.timer = setInterval(() => {
+      this.active = (this.active + 1) % this.total;
+    }, 5000);
+  },
+  beforeDestroy() {
+    clearInterval(this.timer);
   }
-    
-}
+};
 </script>
 <style src="./Home.css" scoped></style>
